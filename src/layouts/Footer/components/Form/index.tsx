7c4f6494fea12f332ddb91a554be714f55f08d23
GalinGdS <61625442+GalinGdS@users.noreply.github.com>
2023-06-22 20:20:05 -0300

    Update index.tsx tentando corrigitr o erro de deplyment

--- src/layouts/Footer/components/Form/index.tsx
+++ src/layouts/Footer/components/Form/index.tsx
@@ -18,8 +18,7 @@ const Form = () => {
     };
         return (
         <form id="myForm" action="https://formsubmit.co/641c71a717626c8b3027597efe1f3491" onSubmit={handleSubmit} method="POST">
-            {name, email, subject, message}
-            <button type="submit">Enviar</button>
+            
         </form>
     );
 
